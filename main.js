6c9671d6f6d61d6b0bcab7389aa787c24a455db5
oliverlibaw <92615450+oliverlibaw@users.noreply.github.com>
2023-04-28 17:56:25 -0700

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const winkDetectedImage = document.getElementById("wink-detected");
 const winkNotDetectedImage = document.getElementById("wink-not-detected");
 
 (async () => {
-  const faceMesh = new faceMesh.FaceMesh();
+  const faceMeshInstance = new faceMesh.FaceMesh();
 
   async function setupCamera() {
     try {
@@ -22,68 +22,4 @@ const winkNotDetectedImage = document.getElementById("wink-not-detected");
     } catch (error) {
       console.error('Error setting up camera:', error);
       alert(
-        'Camera access is required for this app to work. Please enable camera access and reload the page.'
-      );
-    }
-
-    return new Promise((resolve) => {
-      videoElement.onloadedmetadata = () => {
-        videoElement.play();
-        resolve(videoElement);
-      };
-    });
-  }
-
-  await setupCamera();
-  videoElement.onplay = () => {
-    detectWink();
-  };
-
-  function euclideanDistance(point1, point2) {
-    const dx = point2[0] - point1[0];
-    const dy = point2[1] - point1[1];
-    return Math.sqrt(dx * dx + dy * dy);
-  }
-
-  function isWink(leftEyePoints, rightEyePoints, winkThreshold = 0.6) {
-    const leftEyeHeight =
-      (euclideanDistance(leftEyePoints[1], leftEyePoints[5]) +
-        euclideanDistance(leftEyePoints[2], leftEyePoints[4])) /
-      2;
-    const rightEyeHeight =
-      (euclideanDistance(rightEyePoints[1], rightEyePoints[5]) +
-        euclideanDistance(rightEyePoints[2], rightEyePoints[4])) /
-      2;
-
-    return (
-      leftEyeHeight < winkThreshold * rightEyeHeight ||
-      rightEyeHeight < winkThreshold * leftEyeHeight
-    );
-  }
-
-  async function detectWink() {
-    const faces = await faceMesh.estimateFaces({ input: videoElement });
-
-    let winkDetected = false;
-
-    for (const face of faces) {
-      const leftEyePoints = face.annotations.leftEyeIris;
-      const rightEyePoints = face.annotations.rightEyeIris;
-
-      if (isWink(leftEyePoints, rightEyePoints)) {
-        winkDetected = true;
-        break;
-      }
-    }
-
-    if (winkDetected) {
-      winkDetectedImage.hidden = false;
-      winkNotDetectedImage.hidden = true;
-    } else {
-      winkDetectedImage.hidden = true;
-      winkNotDetectedImage.hidden = false;
-    }
-
-    requestAnimationFrame(detectWink);
-  }
-})();
+        'Camera access is required for this app to
