143f29a4c36a8ab066c52c4129e22acbc1012fca
oliverlibaw <92615450+oliverlibaw@users.noreply.github.com>
2023-04-28 17:36:41 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { Camera } from "@mediapipe/camera_utils/camera_utils";
+import { FaceMesh } from "@mediapipe/face_mesh/face_mesh";
+
 const videoElement = document.createElement('video');
 videoElement.style.display = 'none';
 videoElement.setAttribute('playsinline', '');
@@ -7,67 +10,15 @@ const winkDetectedImage = document.getElementById("wink-detected");
 const winkNotDetectedImage = document.getElementById("wink-not-detected");
 
 async function main() {
-  const camera = new Camera(videoElement, {
-    onFrame: async () => {
-      await detectWink();
-    },
-    width: 640,
-    height: 480,
-  });
-  camera.start();
-
-  const faceMesh = new FaceMesh({
-    locateFile: (file) => {
-      return `https://unpkg.com/mediapipe/face_mesh@0.3.162014726/${file}`;
-    },
-  });
-
-  faceMesh.onResults(async (results) => {
-    const face = results.multiFaceLandmarks[0];
-
-    if (!face) {
-      winkDetectedImage.hidden = true;
-      winkNotDetectedImage.hidden = false;
-      return;
-    }
-
-    const leftEyePoints = face.slice(130, 146);
-    const rightEyePoints = face.slice(246, 262);
-
-    if (isWink(leftEyePoints, rightEyePoints)) {
-      winkDetectedImage.hidden = false;
-      winkNotDetectedImage.hidden = true;
-    } else {
-      winkDetectedImage.hidden = true;
-      winkNotDetectedImage.hidden = false;
-    }
-  });
-
-  async function detectWink() {
-    faceMesh.send({ image: videoElement });
-  }
+  //... rest of the code
 }
 
 function euclideanDistance(point1, point2) {
-  const dx = point2[0] - point1[0];
-  const dy = point2[1] - point1[1];
-  return Math.sqrt(dx * dx + dy * dy);
+  //... rest of the code
 }
 
 function isWink(leftEyePoints, rightEyePoints, winkThreshold = 0.6) {
-  const leftEyeHeight =
-    (euclideanDistance(leftEyePoints[1], leftEyePoints[5]) +
-      euclideanDistance(leftEyePoints[2], leftEyePoints[4])) /
-    2;
-  const rightEyeHeight =
-    (euclideanDistance(rightEyePoints[1], rightEyePoints[5]) +
-      euclideanDistance(rightEyePoints[2], rightEyePoints[4])) /
-    2;
-
-  return (
-    leftEyeHeight < winkThreshold * rightEyeHeight ||
-    rightEyeHeight < winkThreshold * leftEyeHeight
-  );
+  //... rest of the code
 }
 
 main();
