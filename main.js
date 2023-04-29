a688afbce8ccf354a00b1e77c2ec0c512536f680
oliverlibaw <92615450+oliverlibaw@users.noreply.github.com>
2023-04-28 17:42:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-import { Camera } from "@mediapipe/camera_utils/camera_utils";
-import { FaceMesh } from "@mediapipe/face_mesh/face_mesh";
-
 const videoElement = document.createElement('video');
 videoElement.style.display = 'none';
 videoElement.setAttribute('playsinline', '');
@@ -10,15 +7,64 @@ const winkDetectedImage = document.getElementById("wink-detected");
 const winkNotDetectedImage = document.getElementById("wink-not-detected");
 
 async function main() {
-  //... rest of the code
-}
+  const { FaceMesh } = await import("@mediapipe/face_mesh/face_mesh");
+  const { Camera } = await import("@mediapipe/camera_utils/camera_utils");
 
-function euclideanDistance(point1, point2) {
-  //... rest of the code
-}
+  const faceMesh = new FaceMesh();
+  const camera = new Camera(videoElement, {
+    onFrame: async () => {
+      await detectWink();
+    },
+    width: 640,
+    height: 480,
+  });
+  camera.start();
 
-function isWink(leftEyePoints, rightEyePoints, winkThreshold = 0.6) {
-  //... rest of the code
+  function euclideanDistance(point1, point2) {
+    const dx = point2[0] - point1[0];
+    const dy = point2[1] - point1[1];
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+  function isWink(leftEyePoints, rightEyePoints, winkThreshold = 0.6) {
+    const leftEyeHeight =
+      (euclideanDistance(leftEyePoints[1], leftEyePoints[5]) +
+        euclideanDistance(leftEyePoints[2], leftEyePoints[4])) /
+      2;
+    const rightEyeHeight =
+      (euclideanDistance(rightEyePoints[1], rightEyePoints[5]) +
+        euclideanDistance(rightEyePoints[2], rightEyePoints[4])) /
+      2;
+
+    return (
+      leftEyeHeight < winkThreshold * rightEyeHeight ||
+      rightEyeHeight < winkThreshold * leftEyeHeight
+    );
+  }
+
+  async function detectWink() {
+    const faces = await faceMesh.estimateFaces({ input: videoElement });
+
+    let winkDetected = false;
+
+    for (const face of faces) {
+      const leftEyePoints = face.annotations.leftEyeIris;
+      const rightEyePoints = face.annotations.rightEyeIris;
+
+      if (isWink(leftEyePoints, rightEyePoints)) {
+        winkDetected = true;
+        break;
+      }
+    }
+
+    if (winkDetected) {
+      winkDetectedImage.hidden = false;
+      winkNotDetectedImage.hidden = true;
+    } else {
+      winkDetectedImage.hidden = true;
+      winkNotDetectedImage.hidden = false;
+    }
+  }
 }
 
 main();
